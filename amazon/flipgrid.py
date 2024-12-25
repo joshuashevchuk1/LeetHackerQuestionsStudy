871f14864cdda735daf50b54d61fa2a46916651a
joshuashevchuk <joshua.shevchuk@ookla.com>
2024-12-24 21:18:43 -0800

    did the flipgrid challenge and had no real issues without. Prolly because I could actually visualize it.

---
+++ amazon/flipgrid.py
@@ -0,0 +1,23 @@
+def flip_by_90_other(grid):
+    flipped = []
+    for i in range(len(grid[0])):
+        new_row = [grid[j][i] for j in range(len(grid))]
+        flipped.append(new_row)
+    print("flipped : " + str(flipped))
+
+grid = [[1,2,3],[4,5,6],[7,8,9]]
+grid2 = [[1,2],[3,4],[5,6]]
+
+# [1.2.3]            [1,4,7]
+# [4,5,6] --> 90 --> [2,5,8]
+# [7,8,9]            [3,6,9]
+
+# [1,2]             [1,3,5]
+# [3,4] --> 90 -->  [2,4,6]
+# [5,6]
+
+flip_by_90_other(grid)
+print("len(grid) : " + str(len(grid)))
+
+flip_by_90_other(grid2)
+print("len(grid) : " + str(len(grid2)))
