2da2367e2c41cfc9a82e940caee2ff7486d4b517
joshuashevchuk <joshua.shevchuk@ookla.com>
2024-12-26 15:14:16 -0800

    finished leetcode 1492.py. Din't actually take too long because the logic was simple.

--- leet/leet1492.py
+++ leet/leet1492.py
@@ -16,14 +16,14 @@ def getFactors(n):
     for i in range(1, n + 1):
         if isFactor(n, i):
             factors.append(i)
-    return factors
+    return sorted(factors)
 
 def getKthFactor(n, k):
     factors = getFactors(n)
     if k <= len(factors):
         return factors[k - 1]
     else:
-        return None
+        return -1
 
 n = 25*2*3*4
 j = 5
@@ -32,3 +32,26 @@ print(factors)
 k = 5
 kthFactor = getKthFactor(n,k)
 print("kth factor : ", kthFactor)
+
+class Solution:
+    def isFactor(self, n,i):
+        if n % i == 0:
+            return True
+        return False
+
+    def getFactors(self, n):
+        factors = []
+        for i in range(1, n + 1):
+            if self.isFactor(n, i):
+                factors.append(i)
+        return sorted(factors)
+
+    def kthFactor(self, n, k):
+        factors = self.getFactors(n)
+        if k <= len(factors):
+            return factors[k - 1]
+        else:
+            return -1
+
+solution = Solution()
+solution.kthFactor(12,3)
