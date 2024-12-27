2076cc7567fef2571055f8e233bb6f0ea6a42739
joshuashevchuk <joshua.shevchuk@ookla.com>
2024-12-26 16:02:38 -0800

    nice little partitionString problem

--- leet/leet2045.py
+++ leet/leet2045.py
@@ -18,3 +18,46 @@ Output: 6
 Explanation:
 The only valid partition is ("s","s","s","s","s","s").
 """
+
+def getMinimumPartitions(string):
+    current_set = set()
+    sets = []
+
+    for char in string:
+        if char in current_set and char not in sets:
+            current_set.clear()
+            sets.append(current_set)
+        current_set.add(char)
+
+    print(sets)
+    return len(sets)+1
+
+
+def getMinimumPartitionsV2(string):
+    partition_count = 1  # Start with one partition
+    current_set = set()  # Set to track unique characters in the current substring
+
+    for char in string:
+        if char in current_set:
+            # Start a new partition if the character is already in the current set
+            partition_count += 1
+            current_set.clear()  # Clear the set to start a new partition
+        current_set.add(char)  # Add the character to the current partition
+
+    return partition_count
+
+class Solution:
+    def partitionString(self, s: str) -> int:
+        sub = ""
+        num = 0
+        for i in s:
+            if i in sub:
+                sub = i
+                num +=1
+            else:
+                sub += i
+        return num+1
+
+string ="abacaba"
+print(getMinimumPartitions(string))
+print(getMinimumPartitionsV2(string))
