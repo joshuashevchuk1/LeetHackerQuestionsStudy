cbf8e05108b53f365c0ec90e9e1f49e8a8db6c82
joshuashevchuk <joshua.shevchuk@ookla.com>
2024-12-28 22:42:51 -0800

    calc problem solving in leet2268.py

--- leet/leet2268.py
+++ leet/leet2268.py
@@ -1,5 +1,7 @@
+# TODO: solve me
+#
 # minimum number of key presses
-
+#
 # You have a keypad with 9 buttons, numbered from 1 to 9, each mapped to lowercase English letters.
 # You can choose which characters each button is matched to as long as:
 #
@@ -22,6 +24,6 @@
 # 3. assign the 2nd 9 to a set with multiplier 2
 # 4. assign the last 9 to a set with multiplier 3.
 # 5. For each char in the string, find it in the 3 arrays and then its multiplier.
-# 6. to do this quickly, assign key press as a dict whose value is mulipler and key is the char.
-# 7. then O(1) on the dict, and add so O(1) + O(n) and the calc time is O(n)
+# 6. to do this quickly, assign key press as a dict whose value is multiplier and key is the char.
+# 7. then O(1) on the dict, and add so O(1) + O(n) and the calc time is O(n log(n)) (due to the sorting step).
 
