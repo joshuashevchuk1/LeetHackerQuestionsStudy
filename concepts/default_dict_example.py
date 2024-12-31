9c72cb2591a96858834abeac72e0c0dfe321b30a
joshuashevchuk <joshua.shevchuk@ookla.com>
2024-12-31 11:12:26 -0800

    added example method for testing

--- concepts/default_dict_example.py
+++ concepts/default_dict_example.py
@@ -1,8 +1,20 @@
 #
 # concept to help out with default lists
 #
+from fiona.crs import defaultdict
+
 
 #
 #
 #
+
+def createDictIn3s(nums):
+    weights = defaultdict()
+    weights[1] = nums[:2]
+    weights[2] = nums[3:5]
+    weights[3] = nums[6:len(nums)]
+    return weights
+
+
+nums = [1,2,3,4,5,6,7,8,9]
 
