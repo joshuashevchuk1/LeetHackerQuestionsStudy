b8389948073244f01208e0a10f8e6968a16bb26f
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-01-07 13:19:03 -0800

    remove on leet27 better solution

--- leet/leet27.py
+++ leet/leet27.py
@@ -3,3 +3,19 @@ class Solution:
         while val in nums:
             nums.remove(val)
         return len(nums)
+
+# better solution using pop
+
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        # Initialize a pointer to iterate through the list
+        index = 0
+        while index < len(nums):
+            if nums[index] == val:
+                # Remove the element by overwriting it with the last element
+                nums[index] = nums[-1]
+                nums.pop()
+            else:
+                # Move the pointer forward
+                index += 1
+        return len(nums)
