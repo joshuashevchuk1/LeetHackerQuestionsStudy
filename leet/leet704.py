77a210534d0de2162ff69a54d36b9d26711ec586
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-01-23 00:20:55 -0800

    leet35 and leet704

--- leet/leet704.py
+++ leet/leet704.py
@@ -21,7 +21,7 @@ class Solution:
 
 # two pointer solution
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def search(self, nums: list[int], target: int) -> int:
 
         l = 0
         r = len(nums)
@@ -38,5 +38,30 @@ class Solution:
                 return r
             else:
                 l += 1
+
+        return -1
+
+# proper solution non recursive
+
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+
+        l = 0
+        r = len(nums)
+
+        if nums[l] == target:
+            return l
+
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
 
         return -1
