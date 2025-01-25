07e68e834699fe440f7dd71d2f0073db6d613c13
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-01-25 14:58:55 -0800

    space complexity can be reduced for 234

--- leet/leet234.py
+++ leet/leet234.py
@@ -24,3 +24,31 @@ class Solution:
             r -= 1
 
         return True
+
+# technically better O(1) space complexity
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # Step 1: Find the middle of the linked list
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Step 2: Reverse the second half of the linked list
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+
+        # Step 3: Compare the first and second half
+        left, right = head, prev
+        while right:  # Only need to check the reversed half
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+
+        return True
