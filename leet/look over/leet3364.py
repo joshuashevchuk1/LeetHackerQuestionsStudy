bdbd27a02407ddc6b863f1740dc25792263a69ac
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-01-26 22:45:29 -0800

    leet3364

--- leet/look over/leet3364.py
+++ leet/look over/leet3364.py
@@ -1,5 +1,5 @@
 class Solution:
-    def minimumSumSubarray(self, nums: List[int], l: int, r: int) -> int:
+    def minimumSumSubarray(self, nums: list[int], l: int, r: int) -> int:
 
         def prefixSum(nums):
             ps = [0]
