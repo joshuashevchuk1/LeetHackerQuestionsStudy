c9bdaf12a00faef61eb01ab533458a6f658788ef
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-01-28 00:24:35 -0800

    solved leet496.py and nextGreater.py

--- concepts/nextGreater.py
+++ concepts/nextGreater.py
@@ -14,3 +14,15 @@ def nextGreater(vals):
             index = stack.pop()
             answer[index] = vals[i]
         stack.append(i)
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        answer = {}
+        stack = []
+
+        for num in nums2:
+            while stack and num > stack[-1]:
+                answer[stack.pop()] = num
+            stack.append(num)
+
+        return [answer.get(num, -1) for num in nums1]
