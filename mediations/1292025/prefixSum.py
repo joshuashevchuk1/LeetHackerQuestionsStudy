29d1b3bf9b076e60503a8693f266abf8605997ee
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-01-29 22:19:03 -0800

    more review on prefix sum and back tracking

--- mediations/1292025/prefixSum.py
+++ mediations/1292025/prefixSum.py
@@ -1,3 +1,4 @@
+
 # prefix sum is defined as
 # sum of the previous element plus the current element at each iteration
 #
@@ -23,28 +24,3 @@ def subarraySum(ps, i, j):
     if i == 0:
         return ps[j]
     return ps[j] - ps[i-1]
-
-def prefixSumArray(nums):
-    prev = 0
-    ps = []
-    for num in nums:
-        prev += num
-        ps.append(prev)
-    return ps
-
-nums = [1,2,3]
-ps = prefixSumArray(nums)
-
-def subarraySumWindowK(ps, k):
-    subarray_sums = []
-    for i in range(k-1, len(ps)):
-        subarray_sum = subarraySum(ps,i,i-k)
-        subarray_sums.append(subarray_sum)
-    return subarray_sums
-
-i = 0
-j = 2
-print(ps)
-sum = subarraySum(ps,i,j)
-print(sum)
-
