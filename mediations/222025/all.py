6292cdeacd78db0ab70c51c29dbfafc56709a429
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-02-02 23:53:15 -0800

    going over all concepts

--- mediations/222025/all.py
+++ mediations/222025/all.py
@@ -1,6 +1,8 @@
 # prefix-sum calc
+import heapq
 
 nums = [1,2,3,4]
+k = 2
 
 def ps(nums):
     prev = 0
@@ -10,4 +12,40 @@ def ps(nums):
         ps.append(prev)
 
 def get_max(ps,j,i):
-    return ps[j-i]
+    if i == 0:
+        return ps[j]
+    return ps[j] - ps[i-1]
+
+# min/max heap
+
+def minHeap(nums,k):
+    min_heap = []
+    for num in nums:
+        heapq.heappush(min_heap,num)
+        if min_heap > k:
+            heapq.heappop(min_heap)
+
+    return min_heap[0]
+
+def maxHeap(nums,k):
+    nums = -1*nums
+    min_heap = []
+    for num in nums:
+        heapq.heappush(min_heap, num)
+        if min_heap > k:
+            heapq.heappop(min_heap)
+
+    return -1*min_heap[0]
+
+max = minHeap(nums,k)
+min = maxHeap(nums,k)
+
+# max sum, two sum
+
+def ksum(nums):
+    cs = ms = nums[0]
+    for num in nums[1:]:
+        cs = max(num, cs + num)
+        ms = max(ms,cs)
+    return ms
+
