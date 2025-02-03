82bc37c55c9b3737ccf64f4e4e6bec7bf716e3f8
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-02-02 23:39:10 -0800

    solved leet215.py

---
+++ leet/leet215.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        min_heap = []
+        l = len(nums)
+        for i in range(l):
+            heapq.heappush(min_heap, nums[i])
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+        return min_heap[0]
