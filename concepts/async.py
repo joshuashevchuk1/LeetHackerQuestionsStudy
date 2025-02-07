5a882b88419ed79ba10dc7aede2dbac0f004ae5d
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-02-07 12:53:07 -0800

    async hello world example

--- concepts/async.py
+++ concepts/async.py
@@ -1,14 +1,11 @@
 import asyncio
 
-from torch.onnx.symbolic_opset11 import gather
-
-
 async def helloWorld():
     print("helloworld")
 
 async def helloWorldN(n):
     tasks = [helloWorld() for i in range(n)]
-    await gather(tasks)
+    await asyncio.gather(*tasks)
 
 
-helloWorldN(5)
+asyncio.run(helloWorldN(5))
