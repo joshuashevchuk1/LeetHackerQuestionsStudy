5099daf634f495f5dd3b2cab41b03409e8eccf89
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-02-12 21:39:36 -0800

    threading example in python

--- udemyPractice/pythonConcurrencyCourse/threadingExample.py
+++ udemyPractice/pythonConcurrencyCourse/threadingExample.py
@@ -4,19 +4,22 @@ import time
 result_lock = threading.Lock()
 
 def calc_sum_of_squares(n,sum_squares):
-    for i in range(n):
-        sum_squares += i ** 2
-    print(sum_squares)
+    with result_lock:
+        for i in range(n):
+            sum_squares += i ** 2
+        print(sum_squares)
 
 def sleep_a_little(seconds):
     time.sleep(seconds)
 
 def main():
-    n = 5
+    start = time.time()
+    n = 100000
     sum_squares = 0
-    a = threading.Thread(calc_sum_of_squares(n,sum_squares))
-    a.run()
-    print(a.is_alive())
+    a = threading.Thread(target=calc_sum_of_squares, args=(n, sum_squares))
+    a.start()
+    finish = time.time()
+    print(abs(start-finish))
 
 
 if __name__ == "__main__":
