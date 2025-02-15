b9a9b391309048452298911ac9d0599e335607ed
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-02-14 17:07:55 -0800

    made the semaphore example clearer

--- concepts/semaphoreExample.py
+++ concepts/semaphoreExample.py
@@ -1,24 +1,27 @@
 import threading
 
-sem = threading.Semaphore(2)  # Allowing 2 concurrent accesses
+def semaphoreExample():
+    sem = threading.Semaphore(2)  # Allowing 2 concurrent accesses
 
-def func1(n):
+    def func1(n):
+        for i in range(n):
+            sem.acquire()
+            print("func1", n)
+            sem.release()
+
+    def func2(n):
+        for i in range(n):
+            sem.acquire()
+            print("func2", n)
+            sem.release()
+
+    n = 5
     for i in range(n):
-        sem.acquire()
-        print("func1", n)
-        sem.release()
+        t1 = threading.Thread(target=func1, args=(1,))
+        t2 = threading.Thread(target=func2, args=(1,))
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
 
-def func2(n):
-    for i in range(n):
-        sem.acquire()
-        print("func2", n)
-        sem.release()
-
-n = 5
-for i in range(n):
-    t1 = threading.Thread(target=func1, args=(1,))
-    t2 = threading.Thread(target=func2, args=(1,))
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
+semaphoreExample()
