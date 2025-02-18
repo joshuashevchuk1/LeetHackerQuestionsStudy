f46a1206fd687f0b0e54cf879556b50eb6256318
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-02-17 20:31:05 -0800

    learned about async in libraries

--- udemyPractice/pythonConcurrencyCourse/asyncForLoops.py
+++ udemyPractice/pythonConcurrencyCourse/asyncForLoops.py
@@ -5,7 +5,10 @@ import time
 
 async def sleep(n):
     print('before ',n)
-    await asyncio.sleep(1) # waits until this coroutine is finished
+    n = max(2,n)
+    for i in range(1,n):
+        yield i
+        await asyncio.sleep(i) # waits until this coroutine is finished
     print('after ', n)
 
 async def hello():
@@ -13,10 +16,8 @@ async def hello():
 
 async def main():
     start = time.time()
-    try:
-        await asyncio.gather(asyncio.wait_for(sleep(1),5), sleep(2), hello()) # runs all these methods in parrell
-    except asyncio.TimeoutError as e:
-        print("got an exception : ",e)
+    async for k in sleep(5):
+        print(k)
     print("diff: ", time.time()-start)
 
 asyncio.run(main())
