0c3b130fa10331d212fdfa3931b7c9e101ce87b5
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-02-18 22:19:49 -0800

    learned about async libs

--- udemyPractice/pythonConcurrencyCourse/asyncLibsAndUrls.py
+++ udemyPractice/pythonConcurrencyCourse/asyncLibsAndUrls.py
@@ -1,9 +1,31 @@
 import asyncio
+import time
+
+import aiohttp
+import requests
+
+async def get_url_response(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text()
 
 
 async def main():
+    start_time = time.time()
     urls = ["https://www.google.com/" for i in range (5)]
     print(urls)
+
+    sync_test_response = []
+    for url in urls:
+        sync_test_response.append(requests.get(url).text)
+
+    tasks = []
+    for url in urls:
+        tasks.append(asyncio.create_task(get_url_response(url)))
+
+    async_text_response = await asyncio.gather(*tasks)
+    print(async_text_response)
+    print("diff : ", time.time() - start_time)
 
 if __name__ == "__main__":
     asyncio.run(main())
