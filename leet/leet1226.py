8b10a289043f177700ce76d26dd9516dacb34a40
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-02-25 12:46:06 -0800

    leet1226 without a modulo

--- leet/leet1226.py
+++ leet/leet1226.py
@@ -1,5 +1,43 @@
 import threading
 
+class DiningPhilosophersNonModulo:
+    def __init__(self):
+        self.forks = [threading.Semaphore(1) for _ in range(5)]
+
+    # call the functions directly to execute, for example, eat()
+    def wantsToEat(self,
+                   philosopher: int,
+                   pickLeftFork: 'Callable[[], None]',
+                   pickRightFork: 'Callable[[], None]',
+                   eat: 'Callable[[], None]',
+                   putLeftFork: 'Callable[[], None]',
+                   putRightFork: 'Callable[[], None]') -> None:
+        left_fork = philosopher
+        right_fork = 4 if philosopher == 0 else philosopher - 1
+
+        # Ensure deadlock prevention: Philosophers pick up forks in a specific order
+        if philosopher != 4:
+            self.forks[right_fork].acquire()
+            pickRightFork()
+            self.forks[left_fork].acquire()
+            pickLeftFork()
+        else:
+            self.forks[left_fork].acquire()
+            pickLeftFork()
+            self.forks[right_fork].acquire()
+            pickRightFork()
+
+        # Eat
+        eat()
+
+        # Release forks
+        self.forks[left_fork].release()
+        self.forks[right_fork].release()
+
+        # Put forks down
+        putLeftFork()
+        putRightFork()
+
 
 class DiningPhilosophers:
     def __init__(self):
