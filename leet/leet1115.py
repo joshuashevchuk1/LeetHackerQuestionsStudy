636976cb5af460986708ad3ef2536a99a92b3aaa
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-02-25 12:32:03 -0800

    solved FooBar but with semaphores

--- leet/leet1115.py
+++ leet/leet1115.py
@@ -1,6 +1,27 @@
 import threading
 
 
+class FooBarSemaphore:
+    def __init__(self, n):
+        self.n = n
+        self.f = threading.Semaphore(1)
+        self.b = threading.Semaphore(0)
+
+    def foo(self, printFoo: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.f.acquire()
+            # printFoo() outputs "foo". Do not change or remove this line.
+            printFoo()
+            self.b.release()
+
+    def bar(self, printBar: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.b.acquire()
+            # printBar() outputs "bar". Do not change or remove this line.
+            printBar()
+            self.f.release()
+
+
 class FooBar:
     def __init__(self, n):
         self.n = n
