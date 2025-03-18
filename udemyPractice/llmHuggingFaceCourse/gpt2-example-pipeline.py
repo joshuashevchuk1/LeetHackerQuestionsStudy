c255486c71f938041e75d2f5cebe25ebaffabc96
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-03-17 23:23:33 -0700

    generate 5 unique sequences

--- udemyPractice/llmHuggingFaceCourse/gpt2-example-pipeline.py
+++ udemyPractice/llmHuggingFaceCourse/gpt2-example-pipeline.py
@@ -5,3 +5,5 @@ pipe = pipeline("text-generation", model="openai-community/gpt2")
 
 
 print(pipe("i have a dog",max_length=30,num_return_sequences=1,truncation=True))
+
+print(pipe("i have a dog",max_length=30,num_return_sequences=5,truncation=True))
