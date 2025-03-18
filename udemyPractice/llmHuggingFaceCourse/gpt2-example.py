3d9059f502a99e17ddc992a897d77c4877dca8cf
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-03-17 23:22:32 -0700

    using pipelines for gpt-2

--- udemyPractice/llmHuggingFaceCourse/gpt2-example.py
+++ udemyPractice/llmHuggingFaceCourse/gpt2-example.py
@@ -4,7 +4,7 @@ tokenizer = AutoTokenizer.from_pretrained("gpt2")
 
 model = AutoModelForCausalLM.from_pretrained("gpt2",pad_token_id=tokenizer.eos_token_id)
 
-model_inputs =  tokenizer("Ripple effect ", return_tensors='pt')
+model_inputs =  tokenizer("I like my dog", return_tensors='pt')
 
 greedy_output = model.generate(**model_inputs,max_new_tokens=50)
 print(tokenizer.decode(greedy_output[0],skip_special_tokens=True))
