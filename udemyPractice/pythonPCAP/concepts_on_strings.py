ee55b064cf2366841a430806523cf9b963d25d9b
joshuashevchuk <joshua.shevchuk@ookla.com>
2026-03-23 23:04:53 -0700

    bit of useless interview information but I'll keep it.

--- udemyPractice/pythonPCAP/concepts_on_strings.py
+++ udemyPractice/pythonPCAP/concepts_on_strings.py
@@ -13,3 +13,4 @@ sample2 = "test"
 
 # for python we can use unicode
 # unicode stores 4 times the amount of data. That is because characters are 32 bits not 8 bits
+# utf-8 is from 8 to 32 bits. So it can be smaller than ascii.
