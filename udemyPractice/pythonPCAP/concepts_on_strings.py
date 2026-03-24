b64335140b117fc6c8656d8b443e58c41d785528
joshuashevchuk <joshua.shevchuk@ookla.com>
2026-03-23 22:56:11 -0700

    writing a small doc on string concepts

---
+++ udemyPractice/pythonPCAP/concepts_on_strings.py
@@ -0,0 +1,10 @@
+
+# both of these are considered strings
+sample1 = 'test'
+sample2 = "test"
+
+# strings in python are encoded
+# they are stored as numbers
+# enter character and space characters are
+# represented by special designated numbers
+# the most widely used encoding in the word is ascii
